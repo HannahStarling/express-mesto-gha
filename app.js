e536962c66f1b93a9f0fde95fdc5920c6dbfee91
Anna Tuban <mshannahstarling@gmail.com>
2022-03-08 02:49:07 +0300

    delete: extra comments

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const { logger } = require('./middlewars/logger');
-// const errorHandler = require('./middlewars/errorHandler');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(logger);
 app.use(express.json()); /* bodyParser in framework */
 
 app.use((req, res, next) => {
