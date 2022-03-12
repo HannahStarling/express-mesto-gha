9052f97177a0dd69238cb29cbb3ba306cfc5f8df
Anna Tuban <mshannahstarling@gmail.com>
2022-03-12 19:14:04 +0300

    feat: add token auth middleware

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,12 @@ const userRouter = require('express').Router();
 const {
   getUsers,
   getUser,
-  createUser,
   updateUser,
   updateUserAvatar,
 } = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
 userRouter.get('/users/:id', getUser);
-userRouter.post('/users', createUser);
 userRouter.patch('/users/me', updateUser);
 userRouter.patch('/users/me/avatar', updateUserAvatar);
 
